7617dbaba1055645ad50c12e77897acaf2e37cf5
Stormfall13 <stormfall228@gmail.com>
2026-02-06 15:50:09 +0500

    fix use effect

--- kvazar-pern-client/src/pages/QaForm.jsx
+++ kvazar-pern-client/src/pages/QaForm.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
